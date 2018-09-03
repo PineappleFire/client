45a298aad0ff7dbe3dbaa67284040e6fdcee3070
Connor Christensen <chriconn@oregonstate.edu>
2018-09-03 16:10:44 -0700

    refactor: denote root of app and sites
    This is so in future cases where gulp needs to access other things in the app, like resources, it
    can do so

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,26 +6,32 @@ var plugin = require('gulp-load-plugins')({
 })
 var browserSync = require('browser-sync').create()
 
-const ROOT = 'app/sites/'
+const ROOT = 'app/'
+const SITES = ROOT + 'sites/'
 const DEST = 'dest/'
 
+
+// get all html and js files
 gulp.task('html-js', function() {
-  return gulp.src([ROOT + '**/*.html', ROOT + '**/*.js'])
+  return gulp.src([SITES + '**/*.html', SITES + '**/*.js'])
     .pipe(plugin.flatten())
     .pipe(gulp.dest(DEST))
 })
+// get all styl files and compile them
 gulp.task('css', function() {
-  return gulp.src(ROOT + '**/*.styl')
+  return gulp.src(SITES + '**/*.styl')
     .pipe(plugin.stylus())
     .pipe(plugin.flatten())
     .pipe(gulp.dest(DEST))
 })
 
+// if any of the html, styl or js files change, run the tasks
 gulp.task('watch', function() {
-  gulp.watch([ROOT + '**/*.html', ROOT + '**/*.js'], gulp.series('html-js'))
+  gulp.watch([SITES + '**/*.html', SITES + '**/*.js'], gulp.series('html-js'))
   gulp.watch('app/**/*.styl', gulp.series('css'))
 })
 
+// reload the browser if any of the files in the destination folder change
 gulp.task('serve', function() {
     browserSync.init({
         server: { baseDir: DEST },
@@ -39,4 +45,5 @@ gulp.task('dev', gulp.series(
   'build',
   gulp.parallel('watch', 'serve')
 ))
+
 gulp.task('default', gulp.parallel('dev'))
